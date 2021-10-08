cdad0a8703ade2af542a8cd5f3d43064c5e65fca
fabioguasta <f.guastapaglia@studenti.unipi.it>
2021-10-08 18:47:51 +0200

    creazione classe ProjectSet da implementare

---
+++ ProjectSet.java
@@ -0,0 +1,6 @@
+/*ProjectSet contiene la lista di tutti i progetti e consente di creare e/o eliminare progetti e restituire tutti i progetti
+di cui fa parte un determinato utente */
+
+public class ProjectSet {
+    
+}
