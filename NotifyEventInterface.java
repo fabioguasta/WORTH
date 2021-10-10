b4c9d7721dfdef4cf33b990231e14f68cce54327
fabioguasta <f.guastapaglia@studenti.unipi.it>
2021-10-10 20:07:05 +0200

    added

---
+++ NotifyEventInterface.java
@@ -0,0 +1,9 @@
+import Utility.Notification;
+
+import java.rmi.*;
+
+public interface NotifyEventInterface extends Remote{
+    
+    //Metodo utilizzato dal server per notificare un evento al client
+    void notifyEvent(Notification notification) throws RemoteException;
+}
