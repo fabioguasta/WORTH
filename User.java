6fb0cde14779fcee5681ebcbe60a289952156e53
fabioguasta <f.guastapaglia@studenti.unipi.it>
2021-10-10 17:16:48 +0200

    aggiunte import

--- User.java
+++ User.java
@@ -1,4 +1,10 @@
+import Utility.Notification;
+import Utility.Utils;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.rmi.RemoteException;
+import java.security.SecureRandom;
 //Rappresenta un utente e contiene username, password, stato ed interfaccia remota del client
 public class User {
     
--- UserSet.java
+++ UserSet.java
@@ -1,3 +1,15 @@
+import Exceptions.MultipleLoginsException;
+import Exceptions.UserAlreadyLoggedException;
+import Exceptions.UserNotFoundException;
+import Utility.Notification;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 //Contiene la lista do tutti gli utenti e permette la gestione di essi (login, logout etc)
 public class UserSet {
     
