dacc46dc667c5cf109c95c5be3dc5df71798abe1
fabioguasta <fabioguastapaglia@gmail.com>
2021-10-12 19:05:36 +0200

    user implementato

--- User.java
+++ User.java
@@ -16,5 +16,70 @@ public class User {
 
     User(){}
 
-    User(String username, String password)
+    User(String username, String password){
+        this.username=username;
+        on=false;
+        SecureRandom rnd= new SecureRandom();
+        byte[] salt= new byte[16];
+        rnd.nextBytes(salt);
+
+        this.saltKey=Utils.byteToBase64(salt);
+        this.password=Utils.sha512(password, this.saltKey);
+    }
+
+    @JsonIgnore
+    public synchronized void notify(Notification notification) throws RemoteException{
+        if(this.clientInterface==null) return;
+        this.clientInterface.notifyEvent(notification);
+    }
+
+    @JsonIgnore
+    public synchronized void setClient(NotifyEventInterface clientInterface){
+        this.clientInterface=clientInterface;
+    }
+
+    @JsonIgnore
+    public boolean login(String password){
+        String hashPass= Utils.sha512(password, this.saltKey);
+        if(this.password.equals(hashPass)){
+            on=true;
+            return true;
+        }
+
+        return false;
+    }
+
+    @JsonIgnore
+    public boolean isOnline(){
+        return on;
+    }
+
+    @JsonIgnore
+    public void setOnline(boolean online){
+        this.on=online;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setUsername(String username){
+        this.username=username;
+    }
+
+    public String getSaltKey(){
+        return saltKey;
+    }
+
+    public void setPassword(String psw){
+        this.password=psw;
+    }
+
+    public void setSaltKey(String saltKey){
+        this.saltKey=saltKey;
+    } 
 }
