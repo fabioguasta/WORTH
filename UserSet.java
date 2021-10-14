b4571e1b288a01e4da1cc459903c354a210cd868
fabioguasta <fabioguastapaglia@gmail.com>
2021-10-14 18:58:25 +0200

    aggiunti alcuni metodi

--- UserSet.java
+++ UserSet.java
@@ -28,4 +28,44 @@ public class UserSet {
         this.users= users;
         this.userKeys=new HashMap<>();
     }
+    
+    public synchronized User getByUsername(String username) throws UserNotFoundException{
+        for (User usr :users){
+            if(usr.getUsername().equals(username)){
+                return usr;
+            }
+        }
+        throw new UserNotFoundException("Nessun utente con questo username trovato:"+ username);
+    }
+
+    //registra un nuovo utente nel sistema e restituisce TRUE se la registrazione avviene con successo, FALSE altrimenti
+    public synchronized boolean register(String username, String psw) throws IOException{
+        for(User usr: users){
+            System.out.println(usr.getUsername());
+            if(usr.getUsername().equals(username)){ return false; }
+        }
+        users.add(new User(username, psw));
+        storage.updateUsers(users);
+        return true;
+    }
+
+    //restituisce la lista di tutti gli utenti
+    public Map<String, Boolean> getUsersList() {
+        Map<String, Boolean> usersList = new HashMap<>();
+        for (User usr : users) {
+            usersList.put(usr.getUsername(), usr.isOnline());
+        }
+        return usersList;
+    }
+
+    //
+    public synchronized void notifyAll(ProjectSet projects){
+        for(User usr:users){
+            try{
+                usr.notify(new Notification(getUsersList(),projects.getChatList(usr.getUsername())));
+            } catch (RemoteException e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
