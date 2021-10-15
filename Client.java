bc634d8d1aa3dc444b44a462a263ad9f450e4111
fabioguasta <fabioguastapaglia@gmail.com>
2021-10-15 17:48:19 +0200

    aggiunte import

--- Client.java
+++ Client.java
@@ -1,3 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import Utility.Esito;
+import Utility.Utils;
+import Utility.Notification;
+import Utility.ChatProcess;
+import Exceptions.UserNotFoundException;
+
 //Classe client che legge i comandi e li inoltra al server
 
 public class Client {
